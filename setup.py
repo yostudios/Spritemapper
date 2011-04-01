917e8c1f6160e90bbca66aa6b9459d8b11f211e8
Ludvig Ericson <ludvig@lericson.se>
2011-04-01 12:14:46 +0200

    Add setup script and related stuff

---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+
+setup(name="spritecss", version="0.5", url="http://yo.se/",
+      author="Yo Studios AB", author_email="opensource@yostudios.se",
+      packages=["spritecss"])
