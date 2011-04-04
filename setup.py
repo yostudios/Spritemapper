322352ce4b6d4295b9a1b3d1123c48f4af19b62d
Ludvig Ericson <ludvig@lericson.se>
2011-04-04 16:15:59 +0200

    Change distribution URL

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 
-setup(name="spritecss", version="0.5", url="http://yo.se/",
+setup(name="spritecss", version="0.5", url="http://yostudios.se/",
       author="Yo Studios AB", author_email="opensource@yostudios.se",
       packages=["spritecss"])
