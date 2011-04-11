8c837a3ebbde0fc04e84b007d34c2f5da4129948
Ludvig Ericson <ludvig@lericson.se>
2011-04-11 07:26:20 +0200

    Add binary `spritecss` to setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
+
+cli_tools = ["spritecss = spritecss.main:main"]
 
 setup(name="spritecss", version="0.5", url="http://yostudios.se/",
       author="Yo Studios AB", author_email="opensource@yostudios.se",
-      packages=["spritecss"])
+      entry_points={"console_scripts": cli_tools},
+      packages=["spritecss", "spritecss.css", "spritecss.packing"])
