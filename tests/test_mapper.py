32be2b4b87d0b326dbc191b4d8125e7489952025
Ludvig Ericson <ludvig@lericson.se>
2011-04-11 13:26:31 +0200

    Shorten mapper testing code

--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -1,29 +1,38 @@
-from nose.tools import eq_
+from nose.tools import nottest, istest, eq_
 from spritecss import SpriteRef
 from spritecss.config import CSSConfig
-from spritecss.mapper import SpriteDirsMapper, mapper_from_conf
+from spritecss.mapper import mapper_from_conf
 
+@nottest
+def test_mapper(f):
+    @istest
+    def inner():
+        (conf, rels) = f()
+        mapper = mapper_from_conf(conf)
+        source = (conf.root or "test") + "/file.css"
+        for sfn, sm_fn in rels.iteritems():
+            res = mapper(SpriteRef(sfn, source=source))
+            assert res == sm_fn, ("%r should map to %r, got %r"
+                                  % (sfn, sm_fn, res))
+    return inner
+
+@test_mapper
 def test_default():
-    rels = {"test/foo/bar.png": "test/foo",
-            "test/foo/blah.png": "test/foo",
-            "test/foo/quux/abc.png": "test/foo/quux",
-            "test/foo.png": "test"}
-    m = SpriteDirsMapper()
-    for sfn, sm_fn in rels.iteritems():
-        res = m(SpriteRef(sfn, source="test/file.css"))
-        eq_(res, sm_fn)
-
-def test_confed_default():
-    conf = CSSConfig(root="test")
     rels = {"test/foo/bar.png": "test/foo.png",
             "test/foo/blah.png": "test/foo.png",
             "test/foo/quux/abc.png": "test/foo/quux.png",
             "test/foo.png": "test.png"}
-    m = mapper_from_conf(conf)
-    for sfn, sm_fn in rels.iteritems():
-        res = m(SpriteRef(sfn, source="test/file.css"))
-        eq_(res, sm_fn)
+    return (CSSConfig(), rels)
 
+@test_mapper
+def test_confed_default():
+    rels = {"test/foo/bar.png": "test/foo.png",
+            "test/foo/blah.png": "test/foo.png",
+            "test/foo/quux/abc.png": "test/foo/quux.png",
+            "test/foo.png": "test.png"}
+    return (CSSConfig(root="test"), rels)
+
+@test_mapper
 def test_confed_no_recurse():
     conf = CSSConfig(base={"recursive": False,
                            "sprite_dirs": "foo"}, root="test")
@@ -32,10 +41,7 @@ def test_confed_no_recurse():
             "test/foo/quux/abc.png": "test/foo.png",
             "test/foo.png": None,
             "test.png": None}
-    m = mapper_from_conf(conf)
-    for sfn, sm_fn in rels.iteritems():
-        res = m(SpriteRef(sfn, source="test/file.css"))
-        eq_(res, sm_fn)
+    return (conf, rels)
 
 def test_confed_single_map():
     conf = CSSConfig(base={"output_image": "sm.png"}, root="test")
@@ -44,7 +50,4 @@ def test_confed_single_map():
             "test/foo/blah.png",
             "test/foo/quux/abc.png",
             "test/foo.png")
-    m = mapper_from_conf(conf)
-    for sfn in sfns:
-        res = m(SpriteRef(sfn, source="test/file.css"))
-        eq_(res, sm_fn)
+    return (conf, dict((sfn, sm_fn) for sfn in sfns))
