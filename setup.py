2b4d32d7f4c1890749188d8ec026b80849afae2a
Ludvig Ericson <ludvig@lericson.se>
2011-04-12 13:44:34 +0200

    Add MIT/X11 license

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ cli_tools = ["spritecss = spritecss.main:main"]
 
 setup(name="spritecss", version="0.5", url="http://yostudios.se/",
       author="Yo Studios AB", author_email="opensource@yostudios.se",
+      description="Do-what-I-mean automatic CSS spritemapper",
+      license="MIT/X11",
       packages=["spritecss", "spritecss.css", "spritecss.packing"],
       test_suite="nose.collector", tests_require=["nose"],
       entry_points={"console_scripts": cli_tools})
