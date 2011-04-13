89b5f2a7afaa5d8533500ab35dd418e996bd305d
jbergstroem <bugs@bergstroem.nu>
2011-04-13 23:19:58 +0200

    Add long description and fix 80w

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+readme_text = open("README.rst", "U").read()
+
 cli_tools = ["spritemapper = spritecss.main:main"]
 
-setup(name="spritemapper", version="0.5", url="http://yostudios.github.com/Spritemapper/",
+setup(name="spritemapper", version="0.5",
+      url="http://yostudios.github.com/Spritemapper/",
       author="Yo Studios AB", author_email="opensource@yostudios.se",
-      description="A suite for merging multiple images and generate corresponding CSS in one go",
+      description="A suite for merging multiple images "
+                  "and generate corresponding CSS in one go",
+      long_description=readme_text,
       license="MIT/X11",
       packages=["spritecss", "spritecss.css", "spritecss.packing"],
       test_suite="nose.collector", tests_require=["nose"],
