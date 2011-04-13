04f74b5b8de5873def5a36da54e9c01e5a34f771
jbergstroem <bugs@bergstroem.nu>
2011-04-13 23:00:58 +0200

    Fix homepage/description

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 cli_tools = ["spritemapper = spritecss.main:main"]
 
-setup(name="spritemapper", version="0.5", url="http://yostudios.se/",
+setup(name="spritemapper", version="0.5", url="http://yostudios.github.com/Spritemapper/",
       author="Yo Studios AB", author_email="opensource@yostudios.se",
-      description="Do-what-I-mean automatic CSS spritemapper",
+      description="A suite for merging multiple images and generate corresponding CSS in one go",
       license="MIT/X11",
       packages=["spritecss", "spritecss.css", "spritecss.packing"],
       test_suite="nose.collector", tests_require=["nose"],
